570a5f3dbcf289b64fca4c38d7710468a77a6378
DanielLowensprung <154324898+DanielLowensprung@users.noreply.github.com>
2025-05-12 09:53:16 +0200

    fix: :zap: Booking card is working now
    the booking card component was broken but now it is fixed
    
    #16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Card from "@/components/card";
+
 export default function HomePage() {
-  return <h2>Booking system</h2>;
+  return (
+    <>
+      <Card
+        title="look at my cat"
+        description="he is so cool."
+        number="2007"
+        imageSRC="https://pbs.twimg.com/media/Fq4OFlOWIAkNcZC.jpg:large"
+        link="https://tillerquest.tiller.blog/shop"
+      />
+    </>
+  );
 }
