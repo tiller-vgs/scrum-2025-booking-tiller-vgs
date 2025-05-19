10e258b24b8e80a565d06936cead284318d5c313
DanielLowensprung <154324898+DanielLowensprung@users.noreply.github.com>
2025-05-19 08:23:35 +0200

    fix: :bug: fix global css
    fixed merge conlicts with global css
    
    #16

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,7 @@
 function Card({ title, number, description, imageSRC, link }) {
   return (
     <a href={link}>
-      <div className=" bg-white rounded-lg p-4 border-3 border-green-700 hover:outline-3 hover:outline-offset-2 hover:outline-green-500 hover:bg-white/70 active:bg-white/90 focus:outline-3 focus:outline-offset-3">
+      <div className=" max-w-175 bg-white rounded-lg p-4 border-3 border-green-700 hover:outline-3 hover:outline-offset-2 hover:outline-green-500 hover:bg-white/70 active:bg-white/90 focus:outline-3 focus:outline-offset-3">
         <h2 className="text-xl font-bold text-black">{title}</h2>
         <p className="text-gray-500">{number}</p>
         <p className="text-gray-700">{description}</p>
