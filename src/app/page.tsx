92c2256092609ae2b9b60c694a4591fbe259a6d2
DanielLowensprung <154324898+DanielLowensprung@users.noreply.github.com>
2025-05-19 08:42:27 +0200

    fix: :bug: merge confilct fix
    #16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import Card from "@/components/card";
-
 export default function HomePage() {
-  return (
-    <>
-      <Card
-        title="look at my cat"
-        description="he is so cool."
-        number="2007"
-        imageSRC="https://pbs.twimg.com/media/Fq4OFlOWIAkNcZC.jpg:large"
-        link="https://tillerquest.tiller.blog/shop"
-      />
-    </>
-  );
+  return <h2>Booking system</h2>;
 }
