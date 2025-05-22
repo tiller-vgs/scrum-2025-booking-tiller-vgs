e2b1faf0595196682658688ab5c24ea2c49ec4e1
VanessaD07 <152720008+VanessaD07@users.noreply.github.com>
2025-05-22 12:35:18 +0200

    feat: add calendar component #31

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+import React from "react";
+import { CalendarForm } from "@/app/Calendar/CalendarForm";
+
 export default function HomePage() {
-  return <h2>Booking system</h2>;
+  return (
+    <main>
+      <h2>Booking system</h2>
+      <CalendarForm />
+    </main>
+  );
 }
